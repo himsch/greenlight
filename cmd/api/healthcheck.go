be7d43d4790a388669ce94fb6ee2a1bf67684af4
song <55rsearchco@gmail.com>
2024-03-30 01:25:26 +0900

    JSON 사용해보기

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,12 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// 이스케이프할 필요 없이 JSON의 문자를 사용합니까?
+	// %q 동사를 사용하여 보간된 값을 큰따옴표로 묶습니다.
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
