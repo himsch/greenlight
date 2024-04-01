5c3e33977a11d25d75fd0eb7abd50109c3b42df2
song <55rsearchco@gmail.com>
2024-04-01 15:24:28 +0900

    UnmarshalJSON 사용해보기

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +17,30 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// 수신되는 JSON 값은 다음 형식의 문자열일 것으로 예상됩니다.
+	// "<runtime> mins", 가장 먼저 해야 할 일은 이 문자열에서 주변의 큰따옴표를 제거하는 것입니다.
+	// 인용을 해제할 수 없으면 ErrInvalidRuntimeFormat 오류가 반환됩니다.
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// 문자열을 split하여 숫자가 포함된 부분을 분리합니다.
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
