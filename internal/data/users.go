cbcb36101e32c24b7722a4778679491730cd392b
song <55rsearchco@gmail.com>
2024-04-09 15:19:43 +0900

    Adding Validation Checks

--- internal/data/users.go
+++ internal/data/users.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"errors"
+	"greenlight/internal/validator"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -46,4 +47,34 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	// 비밀번호 해시가 nil인 경우 이는 코드베이스의 논리 오류로 인한 것입니다
+	// (아마도 사용자에 대한 비밀번호 설정을 잊어버렸기 때문일 것입니다).
+	// 여기에 포함하면 유용한 온전성 검사이지만 클라이언트가 제공하는 데이터에는 문제가 되지 않습니다.
+	// 따라서 검증 맵에 오류를 추가하는 대신 패닉을 발생시킵니다.
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
