aa5e06cc7e469c2be055d0f40a46fbf53f9b761d
song <55rsearchco@gmail.com>
2024-04-09 12:00:58 +0900

    Limiter 리팩토링 (클라이언트 맵 최적화)

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"net/http"
 	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -23,40 +24,61 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// 클라이언트의 IP 주소와 속도 제한기를 보관할 뮤텍스와 맵을 선언합니다.
+	// 각 클라이언트에 대한 속도 제한기와 마지막으로 확인된 시간을 보유하도록 클라이언트 구조체를 정의합니다.
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
 	var (
-		mu      sync.Mutex
-		clients = make(map[string]*rate.Limiter)
+		mu sync.Mutex
+		// 값이 클라이언트 구조체에 대한 포인터가 되도록 맵을 업데이트합니다.
+		clients = make(map[string]*client)
 	)
 
+	// 1분마다 한 번씩 클라이언트 맵에서 오래된 항목을 제거하는 백그라운드 고루틴을 실행합니다.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			// 정리가 진행되는 동안 속도 제한기 검사가 발생하지 않도록 뮤텍스를 잠급니다.
+			mu.Lock()
+
+			// 모든 클라이언트를 반복합니다. 지난 3분 동안 표시되지 않은 경우 지도에서 해당 항목을 삭제하세요.
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+
+			// 중요한 것은 정리가 완료되면 뮤텍스를 잠금 해제하는 것입니다.
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 요청에서 클라이언트의 IP 주소를 추출합니다.
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			app.serverErrorResponse(w, r, err)
 			return
 		}
 
-		// 이 코드가 동시에 실행되지 않도록 뮤텍스를 잠급니다.
 		mu.Lock()
 
-		// 해당 IP 주소가 이미 맵에 존재하는지 확인하세요.
-		// 그렇지 않은 경우 새 속도 제한기를 초기화하고 IP 주소와 제한기를 맵에 추가합니다.
 		if _, found := clients[ip]; !found {
-			clients[ip] = rate.NewLimiter(2, 4)
+			// 아직 존재하지 않는 경우 새 클라이언트 구조체를 생성하여 맵에 추가합니다.
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
 		}
 
-		// 현재 IP 주소에 대한 속도 제한기에서 Allow() 메서드를 호출합니다.
-		// 요청이 허용되지 않으면 이전과 마찬가지로 뮤텍스를 잠금 해제하고 429 Too Many Requests 응답을 보냅니다.
-		if !clients[ip].Allow() {
+		// 클라이언트의 마지막 확인 시간을 업데이트합니다.
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].limiter.Allow() {
 			mu.Unlock()
 			app.rateLimitExceedeResponse(w, r)
 			return
 		}
 
-		// 매우 중요한 것은 체인의 다음 핸들러를 호출하기 전에 뮤텍스를 잠금 해제하는 것입니다.
-		// 뮤텍스를 잠금 해제하기 위해 *defer*를 사용하지 않는다는 점에 유의하십시오.
-		// 이는 이 미들웨어의 다운스트림 모든 핸들러도 반환될 때까지 뮤텍스가 잠금 해제되지 않음을 의미하기 때문입니다.
 		mu.Unlock()
 
 		next.ServeHTTP(w, r)
