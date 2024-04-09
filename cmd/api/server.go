d97d0c121d510af37d02c3a696729a1b8e5f60e9
song <55rsearchco@gmail.com>
2024-04-09 14:29:00 +0900

    Shutdown 메서드와 context 이용해 20초 유예기간과 함께 우아하게 종료하기

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,30 +22,55 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	// shutdownError 채널을 만듭니다. 우리는 이것을 사용하여
+	// Shutdown() 함수에 의해 반환된 오류를 수신합니다.
+	shutdownError := make(chan error)
+
 	go func() {
-		// os.Signal 값을 전달하는 종료 채널을 만듭니다.
 		quit := make(chan os.Signal, 1)
-
-		// signal.Notify()를 사용하여 들어오는 SIGINT 및 SIGTERM 신호를 수신하고 이를 종료 채널에 전달합니다.
-		// 다른 신호는 signal.Notify()에 의해 포착되지 않으며 기본 동작을 유지합니다.
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
-		// 종료 채널에서 신호를 읽으십시오. 이 코드는 신호가 수신될 때까지 Block 됩니다.
 		s := <-quit
 
-		// 신호가 포착되었음을 알리는 메시지를 기록합니다.
-		// 신호 이름을 가져오고 이를 로그 항목 속성에 포함하기 위해 신호에 대해 String() 메서드도 호출합니다.
-		app.logger.PrintInfo("caught signal", map[string]string{
+		// "caught signal" 대신 "shutting down server"으로 로그 항목을 업데이트합니다.
+		app.logger.PrintInfo("shutting down server", map[string]string{
 			"signal": s.String(),
 		})
 
-		// 0(성공) 상태 코드로 애플리케이션을 종료합니다.
-		os.Exit(0)
+		// 20초 제한 시간으로 컨텍스트를 생성합니다.
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
+		// 방금 만든 컨텍스트를 전달하여 서버에서 Shutdown()을 호출합니다.
+		// Shutdown()은 단계적 종료가 성공했거나 오류(리스너를 닫는 데 문제가 있거나
+		// 20초 컨텍스트 기한에 도달하기 전에 종료가 완료되지 않았기 때문에 발생할 수 있음)
+		// 인 경우 nil을 반환합니다. 이 반환 값을 shutdownError 채널에 전달합니다.
+		shutdownError <- srv.Shutdown(ctx)
 	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
 		"env":  app.config.env,
 	})
-	return srv.ListenAndServe()
+
+	// 서버에서 Shutdown()을 호출하면 ListenAndServe()가 즉시 http.ErrServerClosed 오류를 반환하게 됩니다.
+	// 따라서 이 오류가 표시되면 이는 실제로 좋은 것이며 정상적인 종료가 시작되었음을 나타냅니다.
+	// 따라서 우리는 이를 구체적으로 확인하고 http.ErrServerClosed가 아닌 경우에만 오류를 반환합니다.
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	// 그렇지 않으면 Shutdown()에서 반환 값을 받기를 기다립니다.
+	// shutdownError 채널. 반환 값이 오류인 경우 정상적인 종료에 문제가 있음을 알고 오류를 반환합니다.
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	// 이 시점에서 우리는 정상적인 종료가 성공적으로 완료되었음을 확인하고 "서버 중지됨" 메시지를 기록합니다.
+	app.logger.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+
+	return nil
 }
