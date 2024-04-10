475de07de08a9bd0750f93163f06b206e8b36fff
song <55rsearchco@gmail.com>
2024-04-10 13:08:45 +0900

    Creating an email helper

---
+++ internal/mailer/mailer.go
@@ -0,0 +1,70 @@
+package mailer
+
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"time"
+
+	"github.com/go-mail/mail/v2"
+)
+
+//go:embed "templates"
+var templateFS embed.FS
+
+type Mailer struct {
+	dialer *mail.Dialer
+	sender string
+}
+
+func New(host string, port int, username, password, sender string) Mailer {
+	dialer := mail.NewDialer(host, port, username, password)
+	dialer.Timeout = 5 * time.Second
+
+	return Mailer{
+		dialer: dialer,
+		sender: sender,
+	}
+}
+
+func (m Mailer) Send(recipient, templateFile string, data any) error {
+	tmpl, err := template.New("email").ParseFS(templateFS, "templates/"+templateFile)
+	if err != nil {
+		return err
+	}
+
+	subject := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(subject, "subject", data)
+	if err != nil {
+		return err
+	}
+
+	plainBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(plainBody, "plainBody", data)
+	if err != nil {
+		return err
+	}
+
+	htmlBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(htmlBody, "htmlBody", data)
+	if err != nil {
+		return err
+	}
+
+	// AddAlternative() 메서드를 사용하여 HTML 본문을 설정합니다.
+	// AddAlternative()는 항상 SetBody() *후에* 호출되어야 한다는 점에 유의하는 것이 중요합니다.
+	msg := mail.NewMessage()
+	msg.SetHeader("To", recipient)
+	msg.SetHeader("From", m.sender)
+	msg.SetHeader("Subject", subject.String())
+	msg.SetBody("text/plain", plainBody.String())
+	msg.AddAlternative("texthtml", htmlBody.String())
+
+	// SMTP 서버에 대한 연결을 열고 메시지를 보낸 다음 닫습니다.
+	err = m.dialer.DialAndSend(msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
